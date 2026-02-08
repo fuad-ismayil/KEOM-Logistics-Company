b77b8045f0e1ee70bbfe3be3e7e1f123d0c918fc
fuad-ismayil <ismayilfaud004@gmail.com>
2026-02-09 03:10:17 +0400

    i started making services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,9 @@
+import ServicesPageIntro from "@/components/services/ServicesPageIntro";
+export default function Services() {
+    return (
+        <>
+            <ServicesPageIntro />
+        </>
+    );
+}
+    
