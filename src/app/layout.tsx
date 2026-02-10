0ebae282bb1369732f633954629228727b4a89b7
fuad-ismayil <ismayilfaud004@gmail.com>
2026-02-10 21:58:17 +0400

    bg color fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
         <Header />
         <main>{children}</main>
